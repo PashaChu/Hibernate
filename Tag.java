84a4af47828186344d3ef18743f841acd9eba24c
PashaChu <113158505+PashaChu@users.noreply.github.com>
2023-12-24 11:26:56 +0500

    Add files via upload

--- Tag.java
+++ Tag.java
@@ -3,23 +3,21 @@ package com.example.hello_spring;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "tag")
-public class Tag {
-    private  Integer id;
+@Table(name = "tag", schema = "public")
+public class Tag extends User {
     private  String text;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    public Integer getId(){return id;}
-    public void setId(Integer id) {this.id = id;}
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "text")
+    public String getText() {return text;}
+    public void setText(String text) {this.text = text;}
 
-    public String getText(){return text;}
-    public void setText(String text){this.text = text;}
+    @ManyToOne
+    private Post post;
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Tag{" +
-                "id=" + id +
                 ", text='" + text + '\'' +
                 '}';
     }
--- User.java
+++ User.java
@@ -1,29 +1,22 @@
 package com.example.hello_spring;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "user", schema = "public")
 public class User {
-    private Integer id;
+    private Integer id_user;
     private String login;
     private String password;
     private String country;
     private String sex;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "course")
-    @TableGenerator(
-            name="course",
-            table="GENERATOR_TABLE",
-            pkColumnName = "key",
-            valueColumnName = "next",
-            pkColumnValue="course",
-            allocationSize=30
-    )
+    @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "Id")
-    public Integer getId(){return id;}
-    public void setId(Integer id){this.id = id;}
+    public Integer getId(){return id_user;}
+    public void setId(Integer id){this.id_user = id;}
 
     @Column(name = "Login")
     public String getLogin(){return login;}
@@ -41,10 +34,13 @@ public class User {
     public String getSex() {return sex;}
     public void setSex(String sex){this.sex = sex;}
 
+    @OneToMany
+    private List<Post> post;
+
     @Override
     public String toString(){
         return "User{" +
-                "id=" + id +
+                "id=" + id_user +
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
                 ", country='" + country + '\'' +
